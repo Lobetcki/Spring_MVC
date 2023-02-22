c24ab5bf67566709d953aea92d82499f1dcead62
Anton Mikhailouski <Lobetcki@gmail.com>
2023-02-22 10:09:46 +0300

    Spring MVC 22.02.2023

---
+++ src/main/skypro/spring_mvc/controller/SpringMVCController.java
@@ -0,0 +1,21 @@
+package spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+// Создаем класс SpringMVCController и аннотируем его двумя аннотациями:
+// Аннотация Controller, чтобы создался бин класса
+// Аннотация RequestMapping, чтобы обозначить необходимый URL
+@Controller
+@RequestMapping("/skypro")
+public class SpringMVCController {
+
+    // Создаем метод, который только возвращает название отображения,
+    // которое мы создадим далее
+    // А срабатывать этот метод будет, когда URL будет содержать "/skypro/1"
+    @RequestMapping("/1")
+    public String getFirstView() {
+        return "view1";
+    }
+}
